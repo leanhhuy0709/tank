dfb4bdbf8c335dae87a65dc66094c402484802aa
leanhhuy0709 <huy.leanh0709@hcmut.edu.vn>
2023-07-17 09:58:55 +0700

    init

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Candy Rush',
+    url: 'https://github.com/leanhhuy0709/candy-rush',
+    version: '2.0',
+    type: Phaser.AUTO,
+    parent: 'Game',
+    scene: [],
+    input: {
+        keyboard: true,
+    },
+    width: 600,
+    height: 800,
+    backgroundColor: '#000000'
+}
